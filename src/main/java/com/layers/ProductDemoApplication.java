f38bf3efc4bd1e4c29c4a7e117f030aa7860f701
SyedJunaid786 <junaid.pasha@codingmart.com>
2020-02-17 16:18:19 +0530

    change 4 to 6

--- src/main/java/com/layers/ProductDemoApplication.java
+++ src/main/java/com/layers/ProductDemoApplication.java
@@ -13,7 +13,7 @@ public class ProductDemoApplication
 		System.out.println("New Feature 1 Added");
 		System.out.println("New Feature 2 Added");
 		System.out.println("New Feature 3 Added");
-		System.out.println("New Feature 4 Added");
+		System.out.println("New Feature 6 Added");
 		System.out.println("New Feature 9 and extra func Added");
 	}
 
