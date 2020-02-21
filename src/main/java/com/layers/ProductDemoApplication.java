f81b00e832e34cb45e030f83d5b71c22de0850a0
SyedJunaid786 <junaid.pasha@codingmart.com>
2020-02-21 17:30:51 +0530

    changes in f3

--- src/main/java/com/layers/ProductDemoApplication.java
+++ src/main/java/com/layers/ProductDemoApplication.java
@@ -2,6 +2,8 @@ package com.layers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ProductDemoApplication
@@ -13,6 +15,7 @@ public class ProductDemoApplication
 //		System.out.println("In Master Branch");
 //		System.out.println("New Feature 23 Added");
 //		System.out.println("New Feature 24 Added");
+	
 	}
 
 }
